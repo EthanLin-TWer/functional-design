6e0f33d9dd3d6e9d08b3a2cb1773108b740ba623
Ethan <congyu.lin@seraitrade.com>
2024-08-16 20:54:23 +0800

    [sieve] refactor: turning it as mostly functions and leave the class as the entry there

--- src/sieve/sieve.ts
+++ src/sieve/sieve.ts
@@ -1,47 +1,45 @@
-export class Sieve {
-  static primesUpTo(upTo: number): number[] {
-    return Sieve.getPrimes(Sieve.computeSieve(Sieve.makeSieve(Math.max(1, upTo)), 0), [], 0)
+const getPrimes = (sieve: boolean[], primes: number[], n: number): number[] => {
+  if (n >= sieve.length) {
+    return primes
   }
 
-  public static getPrimes(sieve: boolean[], primes: number[], n: number): number[] {
-    if (n >= sieve.length) {
-      return primes
-    }
-
-    if (!sieve[n]) {
-      return Sieve.getPrimes(sieve, [...primes, n], n + 1)
-    }
-
-    return Sieve.getPrimes(sieve, primes, n + 1)
+  if (!sieve[n]) {
+    return getPrimes(sieve, [...primes, n], n + 1)
   }
 
-  public static computeSieve(sieve: boolean[], n: number): boolean[] {
-    if (n >= sieve.length) {
-      return sieve
-    }
+  return getPrimes(sieve, primes, n + 1)
+}
 
-    if (!sieve[n]) {
-      return Sieve.computeSieve(Sieve.markMultiples(sieve, n, 2), n + 1)
-    }
-
-    return Sieve.computeSieve(sieve, n + 1)
-  }
-
-  public static makeSieve(upTo: number): boolean[] {
-    const sieve = new Array(upTo + 1).fill(false)
-    sieve[0] = true
-    sieve[1] = true
+const markMultiples = (sieve: boolean[], prime: number, m: number): boolean[] => {
+  const multiple: number = prime * m
+  if (multiple >= sieve.length) {
     return sieve
   }
 
-  private static markMultiples(sieve: boolean[], prime: number, m: number): boolean[] {
-    const multiple: number = prime * m
-    if (multiple >= sieve.length) {
-      return sieve
-    }
+  const markedSieve = [...sieve]
+  markedSieve[multiple] = true
+  return markMultiples(markedSieve, prime, m + 1)
+}
 
-    const markedSieve = [...sieve]
-    markedSieve[multiple] = true
-    return Sieve.markMultiples(markedSieve, prime, m + 1)
+const computeSieve = (sieve: boolean[], n: number): boolean[] => {
+  if (n >= sieve.length) {
+    return sieve
+  }
+
+  if (!sieve[n]) {
+    return computeSieve(markMultiples(sieve, n, 2), n + 1)
+  }
+
+  return computeSieve(sieve, n + 1)
+}
+
+const makeSieve = (upTo: number): boolean[] => {
+  return [true, true].concat(new Array(upTo - 1).fill(false))
+}
+
+export class Sieve {
+  static primesUpTo(upTo: number): number[] {
+    const sieve = makeSieve(Math.max(1, upTo))
+    return getPrimes(computeSieve(sieve, 0), [], 0)
   }
 }

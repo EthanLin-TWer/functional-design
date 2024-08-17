c367db3ad628774e72a666e770d51d999703140b
Ethan <linesh.simpcity@gmail.com>
2024-08-17 17:13:06 +0800

    [fibonacci] try tco and recursive version of fibonacci

--- src/fibonacci/fibonacci.clj
+++ src/fibonacci/fibonacci.clj
@@ -1,20 +1,42 @@
-(defn fibs-work [n i fs]
+;; tail-call optimized version
+(defn fibs-work-tco [n i fs]
   (if (= i n)
     fs
-    (fibs-work n (inc i) (conj fs (apply + (take-last 2 fs))))))
+    (recur n (inc i) (conj fs (apply + (take-last 2 fs))))))
 
-(defn fibs [n]
+(defn fibs-tco [n]
   (cond
     (< n 1) []
     (= n 1) [1]
-    :else (fibs-work n 2 [1 1])
+    :else (fibs-work-tco n 2 [1 1])
   )
 )
 
-(println (str "Fibs(2): " (fibs 2)))
-(println (str "Fibs(3): " (fibs 3)))
-(println (str "Fibs(4): " (fibs 4)))
-(println (str "Fibs(5): " (fibs 5)))
-(println (str "Fibs(12): " (fibs 12)))
-(println (str "Fibs(15): " (fibs 15)))
-(println (str "Fibs(20): " (fibs 20)))
+(println (str "Fibs(2): " (fibs-tco 2)))
+(println (str "Fibs(3): " (fibs-tco 3)))
+(println (str "Fibs(4): " (fibs-tco 4)))
+(println (str "Fibs(5): " (fibs-tco 5)))
+(println (str "Fibs(12): " (fibs-tco 12)))
+(println (str "Fibs(15): " (fibs-tco 15)))
+(println (str "Fibs(20): " (fibs-tco 20)))
+
+;; recursive version
+(defn fibs-work-rec [n]
+  (cond
+    (< n 1) nil
+    (<= n 2) 1
+    :else (+ (fibs-work-rec (dec n)) (fibs-work-rec (- n 2)))
+  )
+)
+
+(defn fibs-rec [n]
+  (map fibs-work-rec (range 1 (inc n)))
+)
+
+(println (str "Fibs(2): " (vec (fibs-rec 2))))
+(println (str "Fibs(3): " (vec (fibs-rec 3))))
+(println (str "Fibs(4): " (vec (fibs-rec 4))))
+(println (str "Fibs(5): " (vec (fibs-rec 5))))
+(println (str "Fibs(12): " (vec (fibs-rec 12))))
+(println (str "Fibs(15): " (vec (fibs-rec 15))))
+(println (str "Fibs(20): " (vec (fibs-rec 20))))
